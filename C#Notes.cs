dd296b20110923caccb1d79fa69a40d3cda50ed9
Hassan koraim <62600255+HassanKourim@users.noreply.github.com>
2023-03-18 16:30:03 +0200

    Update C#Notes.cs

--- C#Notes.cs
+++ C#Notes.cs
@@ -1,8 +1,11 @@
 // It's some codes that I save it here, to go back to them again
-  // pointer 
+
+// pointer 
+// That's loop to sum the prev. and current value to print.
   for (int c = 0, prev = 0, current = 1; c <= 10; ++c){
     Console.Write(prev + " ");
     int sum = prev + current;
     prev = current;
     current = sum;
   }
+
